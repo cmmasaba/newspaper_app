bdabeb8868e20dbf426c2ea1b22d1bd7239957b2
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-01-10 11:58:24 +0300

    Corrected a type

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,4 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDURECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
